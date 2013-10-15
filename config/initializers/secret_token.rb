84fd64f664cb8906391c290c70789e291de33bc6
Cori Allen <coriallen@gmail.com>
2013-10-15 09:48:16 -0400

    Creates a new first_blog application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstBlog::Application.config.secret_key_base = 'e498f2dc6d1d36729fc07b8be05bd15e86ef50d7a8585e8244d2c172c13a532aac923a70e399e2dc8637212b8fbfb71a3b5c71c65a53d72cbf16f4f4e4ad43b7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FirstBlog::Application.config.session_store :cookie_store, key: '_first_blog_session'
